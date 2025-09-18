e4ead9c32981ae8740fdd20ff217e182aaf404fb
Sreeleena Ganguli <74721433+sreeleenaganguli@users.noreply.github.com>
2025-09-18 12:47:25 +0100

    Update and rename LowerCaseValidationError.java to LowerCaseValidationException.java

--- src/main/java/org/exception/LowerCaseValidationError.java
+++
@@ -1,7 +0,0 @@
-package org.exception;
-
-public class LowerCaseValidationError extends RuleValidationException{
-    public LowerCaseValidationError(String message){
-        super(message);
-    }
-}
---
+++ src/main/java/org/exception/LowerCaseValidationException.java
@@ -0,0 +1,7 @@
+package org.exception;
+
+public class LowerCaseValidationException extends RuleValidationException{
+    public LowerCaseValidationException(String message){
+        super(message);
+    }
+}
