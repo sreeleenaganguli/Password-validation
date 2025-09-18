74564f48b6aaa750058b640492bc5c36c77c6d8e
Sreeleena Ganguli <74721433+sreeleenaganguli@users.noreply.github.com>
2025-09-18 12:45:52 +0100

    Update and rename LengthValidationError.java to LengthValidationException.java

--- src/main/java/org/exception/LengthValidationError.java
+++
@@ -1,7 +0,0 @@
-package org.exception;
-
-public class LengthValidationError extends RuleValidationException{
-    public LengthValidationError(String message){
-        super(message);
-    }
-}
---
+++ src/main/java/org/exception/LengthValidationException.java
@@ -0,0 +1,7 @@
+package org.exception;
+
+public class LengthValidationException extends RuleValidationException{
+    public LengthValidationException(String message){
+        super(message);
+    }
+}
