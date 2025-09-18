3329d7e88848638afd06125bc1c4f5700853e38a
Sreeleena Ganguli <74721433+sreeleenaganguli@users.noreply.github.com>
2025-09-18 12:44:00 +0100

    Update and rename DigitValidationError.java to DigitValidationException.java

--- src/main/java/org/exception/DigitValidationError.java
+++
@@ -1,7 +0,0 @@
-package org.exception;
-
-public class DigitValidationError extends RuleValidationException{
-    public DigitValidationError(String message){
-        super(message);
-    }
-}
---
+++ src/main/java/org/exception/DigitValidationException.java
@@ -0,0 +1,7 @@
+package org.exception;
+
+public class DigitValidationException extends RuleValidationException{
+    public DigitValidationException(String message){
+        super(message);
+    }
+}
